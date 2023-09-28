3ac283df342a59d7ae9328a4f2e0841df7c6a7bc
artasaurrus <yogamart98@gmail.com>
2023-09-28 10:18:12 +0800

    Tugas part 6

--- problem4/main.py
+++ problem4/main.py
@@ -1,45 +1,28 @@
 def find_max_sum_sub_array(k, arr):
-    listmax = 0
-    for i in range(len(arr) - 1):
-        sum = arr[i] + arr[i + 1]
-        if sum > listmax:
-            listmax = sum
-        print(listmax)
-            
-    # for number in arr:
-    #     if number > min(arr):
-    #         listmax.append(number)
-    # maxnum = sum(listmax)
 
-    # result = []
-    #     for j in range (i, len(arr)):
-    #         k += arr [j]
-    #         if k > result[0]:
-    #             result.insert(0, k) 
+    if k <= 0 or k > len(arr):
+            return None
+    
+    highest_sum = float('-inf')
+    sums = sum(arr[:k])
 
-    # if k == arr[-1]:
-    #     for i in range(len(arr) -1):
-    #         if arr[-2] + arr[i] == k:
-    #             result.append(i)
-    #             result.append(len(arr) -2)
-    # else:
-    #     for i in range(len(arr) -1):
-    #         if arr[i] + arr[i + 1] == k:
-    #             result.append(i)
-    #             result.append(i + 1)
-    # return result
+    for i in range(k, len(arr)):
+        sums += arr[i] - arr [i - k]
+        highest_sum = max(highest_sum, sums)
 
-    #     for j in range(arr.index(i) + 1, len(arr)):
-    #         i += arr[j]
-    #         sums.append(i)
-    # if all(j < 0 for j in arr) or not sums:
-    #     return 0
-    # else:
-    #     max_sum = max(sums)
-    #     return max_sum
+        return highest_sum
 
-
-
+    # sums = []
+    # for j, k in enumerate(arr):
+    #     sums.append(k)
+    #     for i in range (j + 1, len(arr)):
+    #         k += arr[i]
+    #         sums.append(k)
+    #     if all(i < 0 for i in arr) or not sums:
+    #         return 0
+    #     else:
+    #         max_sum = max(sums) 
+    #         return max_sum
 
 if __name__ == '__main__':
     print(find_max_sum_sub_array(3, [2, 1, 5, 1, 3, 2])) # 9
